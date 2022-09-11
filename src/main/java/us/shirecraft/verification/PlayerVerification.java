f63837a495aad05d075b94c0432564420cf53f5d
Andy P <andy@silver.sucs.swan.ac.uk>
2022-09-11 15:32:04 +0100

    Add and register command handler (#10)
    * Create and register verify command, resolves #2
    
    * Add tests and refactor packages

--- src/main/java/us/shirecraft/verification/PlayerVerification.java
+++ src/main/java/us/shirecraft/verification/PlayerVerification.java
@@ -1,10 +1,18 @@
-package verification;
+package us.shirecraft.verification;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import us.shirecraft.verification.commands.VerifyCommand;
 
 public class PlayerVerification extends JavaPlugin {
     @Override
     public void onEnable() {
+        PluginCommand verifyCommand = this.getCommand("verify");
+        if(verifyCommand != null)
+        {
+            verifyCommand.setExecutor(new VerifyCommand());
+        }
+
         getLogger().info("Player verification plugin has been enabled");
     }
 
