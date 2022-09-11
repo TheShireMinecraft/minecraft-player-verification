685992edb1251c95ad3e9d952438b6fabdf748d5
Andy Pover <andy@silver.sucs.swan.ac.uk>
2022-09-11 13:55:00 +0100

    Create and register verify command, resolves #2

---
+++ src/main/java/verification/commands/VerifyCommand.java
@@ -0,0 +1,26 @@
+package verification.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class VerifyCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(
+        @NotNull CommandSender sender,
+        @NotNull Command command,
+        @NotNull String label,
+        String[] args
+    ) {
+        if(!(sender instanceof Player player))
+        {
+            sender.sendMessage("This command may only be used by players");
+            return true; // Suppress usage information by returning true
+        }
+        String playerUuid = player.getUniqueId().toString();
+        sender.sendMessage("Hello, player with UUID " + playerUuid + "!");
+        return true;
+    }
+}
