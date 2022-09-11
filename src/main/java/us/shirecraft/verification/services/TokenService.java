e698720c2660fc745306b912c9ae04fcd2a4da4e
Andy Pover <andy@silver.sucs.swan.ac.uk>
2022-09-11 22:27:13 +0100

    Token generation

---
+++ src/main/java/us/shirecraft/verification/services/TokenService.java
@@ -0,0 +1,35 @@
+package us.shirecraft.verification.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.jetbrains.annotations.NotNull;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class TokenService {
+    private final SignatureAlgorithm _algorithm = SignatureAlgorithm.HS256;
+    private final SecretKey _signingKey; // TODO move key to a configuration item
+
+    public TokenService() {
+        _signingKey = Keys.hmacShaKeyFor("-temporary-key-%%-temporary-key-".getBytes()); // 32 chars
+    }
+
+    public String generateTokenForUuid(@NotNull String uuid) {
+        var builder = Jwts.builder();
+        builder.setSubject(uuid);
+        builder.setIssuedAt(new Date());
+        builder.setExpiration(getExpiryDate(10));
+        builder.signWith(_signingKey, _algorithm);
+        return builder.compact();
+    }
+
+    private Date getExpiryDate(int minutesToExpiry) {
+        return Date.from(
+                Instant.now().plus(minutesToExpiry, ChronoUnit.MINUTES)
+        );
+    }
+}
