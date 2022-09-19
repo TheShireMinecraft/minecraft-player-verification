b291ee0edd86fc5e03e3a65beb606206d814b036
Andy Pover <andy@silver.sucs.swan.ac.uk>
2022-09-19 17:22:21 +0100

    Test to confirm #28 is user error

--- src/test/java/us/shirecraft/verification/services/TokenServiceTest.java
+++ src/test/java/us/shirecraft/verification/services/TokenServiceTest.java
@@ -1,12 +1,14 @@
 package us.shirecraft.verification.services;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.SignatureException;
 import org.junit.jupiter.api.Test;
+import us.shirecraft.verification.helpers.UrlHelper;
 import us.shirecraft.verification.models.PluginConfiguration;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TokenServiceTest {
     @Test
@@ -36,5 +38,40 @@ public class TokenServiceTest {
         // Assert
         assertEquals(playerUuid, jwtClaims.getSubject());
         assertEquals(config.tokenExpiryInMinutes, tokenLifeInMinutes);
+    }
+
+    @Test
+    void generateTokenForUuid__should_generate_token_for_url_that_can_be_decoded_and_verified() {
+        // Arrange
+        var config = new PluginConfiguration();
+        config.tokenSigningKey = "-temporary-key-%%-temporary-key-";
+        config.tokenExpiryInMinutes = 10;
+        var systemUnderTest = new TokenService(config);
+        var playerUuid = "069a79f4-44e9-4726-a5be-fca90e38aaf5";
+        var token = systemUnderTest.generateTokenForUuid(playerUuid);
+        var verificationUrl = UrlHelper.buildUrlForToken(
+                "https://localhost:8081/verify/",
+                token,
+                true,
+                true,
+                true
+        );
+        var header = "eyJhbGciOiJIUzI1NiJ9";
+        var urlParts = verificationUrl.split("/");
+        var urlPayload = urlParts[4];
+        var urlSignature = urlParts[5];
+        var reconstructedTokenParts = new String[] { header, "eyJ" + urlPayload, urlSignature };
+
+        // Act
+        var reconstructedToken = String.join(".", reconstructedTokenParts);
+
+        // Assert
+        assertEquals(token, reconstructedToken);
+        assertDoesNotThrow(() -> {
+            Jwts.parserBuilder()
+                    .setSigningKey(config.tokenSigningKey.getBytes())
+                    .build()
+                    .parseClaimsJws(reconstructedToken);
+        });
     }
 }
